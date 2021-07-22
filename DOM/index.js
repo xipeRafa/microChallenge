72ba7009b68ee416d8856ac5d4d2483589c1f064
rafa <rafaelsistem87@gmail.com>
2021-07-22 07:01:10 -0700

    DOM 1

---
+++ DOM/index.js
@@ -0,0 +1,9 @@
+function definirAtributo(){
+    var reftabla=document.getElementById('tabla1');
+    reftabla.setAttribute('border','5');
+  }
+
+  function borrarAtributo(){
+      var reftabla=document.getElementById('tabla1');
+      reftabla.removeAttribute('border');   
+  }
